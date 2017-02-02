a2943436c9621232ba649f38b1cd81cc19bd49f2
John Tran <johnjtran@icloud.com>
2017-02-01 19:30:03 -0800

    Fix tabs

--- src/edu/calstatela/cs/jtran/lab2/SumNumber.java
+++ src/edu/calstatela/cs/jtran/lab2/SumNumber.java
@@ -2,28 +2,28 @@ package edu.calstatela.cs.jtran.lab2;
 
 public class SumNumber {
 
-	public static void main(String[] args) {
-		int v = (new SumNumber()).calc(1);
-		System.out.println(v);
-	}
-	
-	public static final int N = 4;
-	
-	/*
-	 * base case: number = N
-	 * induction step: increment number, 
-	 *    add number to fn with incremented number
-	 *    return value from above
-	 * 
-			print(number + 1);	 */
-	
-	int calc(final int number) {
-		// base case
-		if (number == N) {
-			return number;
-		} else {
-			return number + calc(number + 1);
-		}
-	}
-	
+  public static void main(String[] args) {
+    int v = (new SumNumber()).calc(1);
+    System.out.println(v);
+  }
+
+  public static final int N = 4;
+
+  /*
+   * base case: number = N
+   * induction step: increment number,
+   *    add number to fn with incremented number
+   *    return value from above
+   *
+      print(number + 1);   */
+
+  int calc(final int number) {
+    // base case
+    if (number == N) {
+      return number;
+    } else {
+      return number + calc(number + 1);
+    }
+  }
+
 }
