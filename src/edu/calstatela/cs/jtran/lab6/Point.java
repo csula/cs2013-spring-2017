78c775d46dce9629f3b250bbfb61680d8c588790
John Tran <johnjtran@icloud.com>
2017-02-16 19:55:33 -0800

    Add lab 6

---
+++ src/edu/calstatela/cs/jtran/lab6/Point.java
@@ -0,0 +1,14 @@
+package edu.calstatela.cs.jtran.lab6;
+
+public class Point {
+	public Point(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+	
+	public String toString() {
+		return "(" + x + "," + y + ")";
+	}
+	int x;
+	int y;
+}
