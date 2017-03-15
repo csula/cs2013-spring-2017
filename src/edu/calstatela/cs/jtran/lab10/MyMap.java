f25a1becbfcdfe704efe1c520c6019e7c9e5e2bc
John Tran <johnjtran@icloud.com>
2017-03-14 19:04:35 -0700

    Make mymap generic

--- src/edu/calstatela/cs/jtran/lab10/MyMap.java
+++ src/edu/calstatela/cs/jtran/lab10/MyMap.java
@@ -2,26 +2,26 @@ package edu.calstatela.cs.jtran.lab10;
 
 import java.util.ArrayList;
 
-public class MyMap {
+public class MyMap<K,V> {
 
-	public void insert(Node<String,Integer> node) {
+	public void insert(Node<K,V> node) {
 		if (!exist(node.key)) {
 			collection.add( node );
 		} else {
-			Node<String,Integer> n = getNode( node.key );
+			Node<K,V> n = getNode( node.key );
 			n.val = node.val;
 		}
 	}
 	
-	public void remove(String key) {
-		Node<String,Integer> n = getNode(key);
+	public void remove(K key) {
+		Node<K,V> n = getNode(key);
 		if(n != null) {
 			collection.remove(n);
 		}
 	}
 	
-	public Node<String,Integer> getNode(String key) {
-		for (Node<String,Integer> n : collection) {
+	public Node<K,V> getNode(K key) {
+		for (Node<K,V> n : collection) {
 			if (key.equals (n.key)) {
 				return n;
 			}
@@ -29,8 +29,8 @@ public class MyMap {
 		return null;
 	}
 	
-	public Integer getVal(String key) {
-		Node<String,Integer> n = getNode(key);
+	public V getVal(K key) {
+		Node<K,V> n = getNode(key);
 		if (n != null) {
 			return n.val;
 		} else {
@@ -38,14 +38,23 @@ public class MyMap {
 		}
 	}
 
-	public boolean exist(String key) {
+	public boolean exist(K key) {
 		return getNode(key) != null;
 	}
 	
-	private ArrayList<Node<String,Integer>> collection = 
-			new ArrayList<Node<String,Integer>>();
+	private ArrayList<Node<K,V>> collection = 
+			new ArrayList<Node<K,V>>();
 
 	public static void main(String[] args) {
-
+		MyMap<String,Integer> map = new MyMap<String,Integer>();
+		map.insert( new Node<String,Integer>("john", 56) );
+		map.insert( new Node<String,Integer>("george", 21) );
+		map.insert( new Node<String,Integer>("elvira", 21) );
+		map.insert( new Node<String,Integer>("kevin", 12) );
+		map.insert( new Node<String,Integer>("john", 52) );
+		
+		for (Node<String,Integer> n : map.collection) {
+			System.out.println(n);
+		}
 	}
 }
