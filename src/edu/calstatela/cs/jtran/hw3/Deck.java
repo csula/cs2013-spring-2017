554f8f8f769b424a00132e2a4f234f1a4fd6cfd3
John Tran <johnjtran@icloud.com>
2017-04-18 20:04:48 -0700

    Add lab hw3

---
+++ src/edu/calstatela/cs/jtran/hw3/Deck.java
@@ -0,0 +1,72 @@
+package edu.calstatela.cs.jtran.hw3;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Deck<E extends Comparable<E>> {
+
+	public void addCard(E card) {
+		dataCollection.add(card);
+	}
+	
+	public void shuffle() {
+		Collections.shuffle(dataCollection);
+	}
+	
+	public void Sort() {
+		// Implement sorting on Collections
+	}
+	
+	public boolean isEqual(Deck<E> otherDeck) {
+		return true;
+	}
+	
+	public int[] search() {
+		int a[] = { 0, 0 };
+		return search(a, 1);
+	}
+
+	public int[] search(final int[] maxMin, final int position) {
+		if (position == dataCollection.size() - 1) {
+			return maxMin;
+		} else {
+			E current = dataCollection.get(position);
+			E maxItem = dataCollection.get(maxMin[0]);
+			E minItem = dataCollection.get(maxMin[1]);
+			
+			int newMax = maxMin[0];
+			int newMin = maxMin[1];
+			
+			if (current.compareTo(maxItem) == 1) {
+				newMax = position;
+			}
+			
+			if (current.compareTo(minItem) == -1) {
+				newMin = position;
+			}
+			
+			final int[] a = { newMax, newMin };
+			return search(a, position + 1);
+		}
+	}
+	
+	public void printPermutation(int count) {
+		// implement/call a recursive permutation fn
+	}
+	
+	public ArrayList<E> getCards() {
+		return dataCollection;
+	}
+	
+	private ArrayList<E> dataCollection = new ArrayList<E>();
+
+	public Card removeRear() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void insertFront(Card card) {
+		// TODO Auto-generated method stub
+	}
+
+}
