f368ca4dc0b747011f366751ddeafedc945f3e7d
John Tran <johnjtran@icloud.com>
2017-04-20 19:15:02 -0700

    Update lab 13 will full tree

--- src/edu/calstatela/cs/jtran/lab13/Tree.java
+++ src/edu/calstatela/cs/jtran/lab13/Tree.java
@@ -1,28 +1,68 @@
 package edu.calstatela.cs.jtran.lab13;
 
 public class Tree {
-	void add(TreeNode node) {
-		
-	}
-	
 	void preOrder(TreeNode node) {
-		System.out.println(node);
+		if (node == null) { return ; }
+		System.out.print(node + " ");
 		preOrder(node.L);
 		preOrder(node.R);
 	}
 	
 	void inOrder(TreeNode node) {
+		if (node == null) { return ; }
 		inOrder(node.L);
-		System.out.println(node);
+		System.out.print(node + " ");
 		inOrder(node.R);
 	}
 	
-	void postOrder() {
-		
+	void postOrder(TreeNode node) {
+		if (node == null) { return ; }
+		postOrder(node.L);
+		postOrder(node.R);
+		System.out.print(node + " ");
 	}
 	
+	TreeNode insert(TreeNode h, int val) {
+		if (h == null) {
+			return new TreeNode(val);
+		}
+		int cmp = val - h.val;
+		if (cmp == 0) {
+			h.val = val; 
+		} else if (cmp < 0) {
+		    h.L = insert(h.L, val);
+		} else {
+		    h.R = insert(h.R, val);
+		}
+		return h;
+	}
+
+	int rand(int min, int max) {
+		int a = (int)(Math.random() * max + min);
+		System.out.print(a + " ");
+		return a;
+	}
+	
+	TreeNode root = null;
+	
 	public void run() {
+		root = insert( null, rand(1,10) );
+		for (int i = 0; i < 10; i++) {
+			insert( root, rand(1,10) );
+		}
+
+		System.out.println("");
+		System.out.println("--");
+		preOrder(root);
+		System.out.println("");
+
+		System.out.println("--");
+		postOrder(root);
+		System.out.println("");
 		
+		System.out.println("--");
+		inOrder(root);
+		System.out.println("");
 	}
 	
 	public static void main(String args[]) {
--- src/edu/calstatela/cs/jtran/lab13/TreeNode.java
+++ src/edu/calstatela/cs/jtran/lab13/TreeNode.java
@@ -4,6 +4,11 @@ public class TreeNode {
 	public int val;
 	TreeNode L;
 	TreeNode R;
+	
+	public TreeNode(int val) {
+		this.val = val;
+	}
+	
 	public String toString() {
 		return "" + val;
 	}
