c43840d20d9144e9cfe9299c7804b73fce584913
John Tran <johnjtran@icloud.com>
2017-05-11 13:58:03 -0700

    Provide starter for homework 4

--- src/edu/calstatela/cs/jtran/hw2/App.java
+++ src/edu/calstatela/cs/jtran/hw2/App.java
@@ -1,35 +1,38 @@
 package edu.calstatela.cs.jtran.hw2;
 
 public class App {
-	
+
 	public void runTest(int count) {
 		for (int kk = 0; kk < count; kk++) {
 			for (int ii = 0; ii < count; ii++ ) {
 				Deck<Card> deck = new Deck<Card>();
-				
+
 				// add 52 cards to the deck
 				for (int i = 0; i < 52; i++) {
 					deck.addCard( new Card(i) );
 				}
-		
+
 				for (int j = 0; j < 20; j++) {
 					deck.shuffle();
 				}
-				
+
 				// print all cards in the deck
 				for (Card c : deck.getCards() ) {
-					// System.out.println(c);
+					System.out.println(c);
 				}
 			}
 		}
 	}
-	
-	public static void main(String[] args) {
-		long start = System.currentTimeMillis();
-		new App().runTest(200);
-		long stop = System.currentTimeMillis();
-		long total = stop - start;
-		System.out.println("time: " + total);
+
+	public void run(final int m, final int n, final int k) {
+
 	}
-	
+
+	public static void main(String[] args) {
+		final int n = 20;
+		final int m = 5;
+		final int k = 5;
+		new App().run(m, n, k);
+	}
+
 }
--- src/edu/calstatela/cs/jtran/hw4/Tree.java
+++ src/edu/calstatela/cs/jtran/hw4/Tree.java
@@ -1,14 +1,16 @@
 package edu.calstatela.cs.jtran.hw4;
 
+import java.util.ArrayList;
+
 public class Tree {
 	static final boolean RED = true;
 	static final boolean BLACK = false;
 
 	class Node {
 		public int val;
-		Node left;
-		Node right;
-		boolean color;
+		private Node left;
+		private Node right;
+		private boolean color;
 
 		public Node(int val, boolean color) {
 			this.val = val;
@@ -45,34 +47,13 @@ public class Tree {
 		return x;
 	}
 
-	void preOrder(Node node) {
-		if (node == null) {
-			return;
-		}
-		System.out.print(node + " ");
-		preOrder(node.left);
-		preOrder(node.right);
+	private boolean isRed(Node x) {
+		if (x == null)
+			return false;
+		return (x.color == RED);
 	}
 
-	void inOrder(Node node) {
-		if (node == null) {
-			return;
-		}
-		inOrder(node.left);
-		System.out.print(node + " ");
-		inOrder(node.right);
-	}
-
-	void postOrder(Node node) {
-		if (node == null) {
-			return;
-		}
-		postOrder(node.left);
-		postOrder(node.right);
-		System.out.print(node + " ");
-	}
-
-	Node insert(Node h, int val) {
+	private Node insert(Node h, int val) {
 		if (h == null) {
 			return new Node(val, RED);
 		}
@@ -101,42 +82,74 @@ public class Tree {
 		return h;
 	}
 
-	private boolean isRed(Node x) {
-		if (x == null)
-			return false;
-		return (x.color == RED);
+	private Node remove(Node h, int val) {
+		/* implement the RB Treed node removal here */		
+		return h;
 	}
-
-	int rand(int min, int max) {
+	
+	private int rand(int min, int max) {
 		int a = (int) (Math.random() * max + min);
-		System.out.print(a + " ");
 		return a;
-	}
-
-	Node root = null;
-
-	public void run() {
-		root = insert(null, 1);
-		for (int i = 0; i < 10; i++) {
-			root = insert(root, i);
-			root = insert(root, i * 2);
-		}
-		
-		System.out.println("");
-		System.out.println("--");
-		preOrder(root);
-		System.out.println("");
-
-		System.out.println("--");
-		postOrder(root);
-		System.out.println("");
-
-		System.out.println("--");
-		inOrder(root);
-		System.out.println("");
 	}
 
 	public static void main(String args[]) {
 		new Tree().run();
+	}
+
+	public void run() {
+		Node root = insert(null, 1);
+		
+		ArrayList<Integer> tracker = new ArrayList<Integer>();
+		for (int i = 0; i < 10; i++) {
+			final int n = rand(1, 100);
+			tracker.add(n);
+			root = insert(root, n);
+		}
+		
+		System.out.println("preorder: ");
+		preOrder(root);
+		System.out.println("");
+
+		System.out.println("postorder: ");
+		postOrder(root);
+		System.out.println("");
+
+		System.out.println("inorder: ");
+		inOrder(root);
+		System.out.println("");
+		
+		for (Integer n : tracker) {
+			root = remove(root, n);
+			System.out.println("Removing: " + n);
+			inOrder(root);
+			System.out.println("");
+		}
+	}
+	
+	void preOrder(Node node) {
+		if (node == null) {
+			return;
+		}
+		System.out.print(node + " ");
+		preOrder(node.left);
+		preOrder(node.right);
+	}
+
+	void inOrder(Node node) {
+		if (node == null) {
+			return;
+		}
+		inOrder(node.left);
+		System.out.print(node + " ");
+		inOrder(node.right);
+	}
+
+	void postOrder(Node node) {
+		if (node == null) {
+			return;
+		}
+		postOrder(node.left);
+		postOrder(node.right);
+		System.out.print(node + " ");
 	}
 }
